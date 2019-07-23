b2cdfeffab1873d79287ebb5dcc9de8b8b184a33
JoseC <jcabral@codestack.co>
2019-07-22 18:43:35 -0700

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './views/side-bar/side-bar.component';
+import { HomeComponent } from './views/home/home.component';
+import { AboutComponent } from './views/about/about.component';
+import { ContactComponent } from './views/contact/contact.component';
+import { ResumeComponent } from './views/resume/resume.component';
+import { PortfolioComponent } from './views/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ResumeComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
