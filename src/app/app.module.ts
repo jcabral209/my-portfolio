3338670c6b1f1a62c4191a67aadeb4911ffc9ae6
Jose Cabral <jcabral@codestack.co>
2019-07-24 13:56:43 -0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SideBarComponent } from './views/side-bar/side-bar.component';
+import { SideBarComponent } from './share/side-bar/side-bar.component';
 import { HomeComponent } from './views/home/home.component';
 import { AboutComponent } from './views/about/about.component';
 import { ContactComponent } from './views/contact/contact.component';
 import { ResumeComponent } from './views/resume/resume.component';
 import { PortfolioComponent } from './views/portfolio/portfolio.component';
 import { BannerComponent } from './share/banner/banner.component';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
+// import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   declarations: [
@@ -27,7 +27,7 @@ import { AngularFontAwesomeModule } from 'angular-font-awesome';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    AngularFontAwesomeModule
+    // AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
